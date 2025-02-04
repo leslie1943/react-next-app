ce7aa3051c967cd413764e6eba33d438208832f4
leslie1943 <leslie43@sina.com>
2025-02-04 16:43:56 +0800

    others

--- src/app/(hero)/contact/page.tsx
+++ src/app/(hero)/contact/page.tsx
@@ -5,12 +5,12 @@ import { SiWechat } from "react-icons/si";
 import { MdEmail } from "react-icons/md";
 import { FaMobileRetro } from "react-icons/fa6";
 
-const Contact = () => {
+const ContactHeader = () => {
   return (
     <div>
       <div className="flex items-center">
         <FaMobileRetro size={42} className="mr-4" />
-        <div className="text-3xl">13478452949</div>
+        <div className="text-3xl">(+86) 13478452949</div>
       </div>
       <div className="text-2xl mt-6 flex items-center">
         <SiWechat size={42} className="mr-4" />
@@ -26,6 +26,44 @@ const Contact = () => {
   );
 };
 
+const ContactDetail = () => {
+  return (
+    <div className="text-white text-sm">
+      <div className="flex items-center justify-center m-8">
+        <div className="w-2/5 border-2 border-solid border-white rounded-md p-4">
+          <li className="mt-4">
+            Having been engaged in front-end development for many years, I
+            possess rich project experience ranging from native JavaScript to
+            jQuery, and then to the series of frameworks like
+            <span className="text-green-400"> Vue / React / Angular </span> as
+            well as cross-platform applications.
+          </li>
+          <li className="mt-4">
+            I maintain a <span className="text-green-400"> HIGH </span> level of
+            sensitivity and attention to technologies, and will set up test
+            environments for new functions so that they can be applied to
+            existing projects more quickly
+          </li>
+          <li className="mt-4">
+            I'm familiar with the product development process and have strong
+            abilities in product planning, requirement analysis, team
+            <span className="text-green-400"> Collaboration</span> and
+            <span className="text-green-400"> Communication</span>.
+          </li>
+          <li className="mt-4">
+            I have got promotion opportunities in different companies.
+            <span className="text-green-400 p-1">Attitude</span> is everything.
+          </li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function Page() {
-  return <Hero imgUrl={imgSrc} altTxt="Scale Image" content={Contact()}></Hero>;
+  return (
+    <Hero imgUrl={imgSrc} altTxt="Scale Image" content={ContactHeader()}>
+      <ContactDetail />
+    </Hero>
+  );
 }
