ec723149560975496d8b373f852fcdda14eafb66
leslie1943 <leslie43@sina.com>
2025-02-05 17:41:11 +0800

    projects

--- src/app/(hero)/skills/page.tsx
+++ src/app/(hero)/skills/page.tsx
@@ -1,10 +1,45 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import imgSrc from "../../../../public/skills.jpg";
 import Hero from "@/components/hero";
 import { Skills } from "@/components/skill";
 
+const ViewOptions = (props: { emitChange: Function }) => {
+  const [value, setValue] = useState(1);
+
+  const onChange = (value: number) => {
+    setValue(value);
+    props.emitChange(value);
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div
+        className={`border-2 cursor-pointer border-solid rounded-md border-white p-2 ${
+          value === 1 ? "bg-green-500 text-white" : "bg-white text-green-500"
+        }`}
+        onClick={() => onChange(1)}
+      >
+        Card View
+      </div>
+      <div
+        className={`border-2 cursor-pointer border-solid rounded-md border-white p-2 ml-2 ${
+          value === 2 ? "bg-green-500 text-white" : "bg-white text-green-500"
+        }`}
+        onClick={() => onChange(2)}
+      >
+        Text View
+      </div>
+    </div>
+  );
+};
+
 export default function Page() {
+  const [viewOption, setViewOption] = useState(1);
+  const viewOnChange = (value: number) => {
+    setViewOption(value);
+  };
+
   const skill_set_1 = [
     { id: 1, name: "JavaScript", ability: 5, bgColor: "bg-teal-600" },
     { id: 2, name: "TypeScript", ability: 5, bgColor: "bg-purple-600" },
@@ -43,21 +78,83 @@ export default function Page() {
 
   return (
     <Hero imgUrl={imgSrc} altTxt="Performance Image" content="">
-      <div className="flex justify-between items-center m-8 border-solid -mt-2">
-        <Skills key={1} skills={skill_set_1}></Skills>
+      <div className="-mt-2 flex justify-center">
+        <ViewOptions emitChange={viewOnChange}></ViewOptions>
       </div>
+      {viewOption === 1 ? (
+        <>
+          <div className="flex justify-between items-center m-8 border-solid">
+            <Skills key={1} skills={skill_set_1}></Skills>
+          </div>
 
-      <div className="flex justify-between items-center m-8 border-solid">
-        <Skills key={2} skills={skill_set_2}></Skills>
-      </div>
+          <div className="flex justify-between items-center m-8 border-solid">
+            <Skills key={2} skills={skill_set_2}></Skills>
+          </div>
 
-      <div className="flex justify-between items-center m-8 border-solid">
-        <Skills key={3} skills={skill_set_3}></Skills>
-      </div>
+          <div className="flex justify-between items-center m-8 border-solid">
+            <Skills key={3} skills={skill_set_3}></Skills>
+          </div>
 
-      <div className="flex justify-between items-center m-8 border-solid">
-        <Skills key={4} skills={skill_set_4}></Skills>
-      </div>
+          <div className="flex justify-between items-center m-8 border-solid">
+            <Skills key={4} skills={skill_set_4}></Skills>
+          </div>
+        </>
+      ) : (
+        <div className="flex justify-center mt-10">
+          <div className="text-white border-2 border-solid rounded-lg p-14">
+            <div className="mt-2">
+              Fundamental:
+              <i className="ml-2 text-lime-200">
+                JavaScript, TypeScript, CSS3/Sass/Scss/Tailwind
+              </i>
+            </div>
+            <div className="mt-2">
+              Web Framework:
+              <i className="ml-2 text-orange-500">
+                Vue2 / Vue3, React,Next.js,Angular
+              </i>
+            </div>
+            <div className="mt-2">
+              Multi-Platform:
+              <i className="ml-2 text-indigo-400">
+                Uni-app, Weex, Taro, Flutter
+              </i>
+            </div>
+            <div className="mt-2">
+              Node Framework:
+              <i className="ml-2 text-red-300">NestJs, Koa, Egg, TypeORM</i>
+            </div>
+            <div className="mt-2">
+              API Framework:
+              <i className="ml-2 text-pink-500">GraphQL, Java SpringBoot</i>
+            </div>
+            <div className="mt-2">
+              Build Tools:
+              <i className="ml-2 text-fuchsia-400">Webpack, Vite, Rollup</i>
+            </div>
+            <div className="mt-2">
+              Test Framework:
+              <i className="ml-2 text-purple-400">Jest, Vitest, Cypress</i>
+            </div>
+            <div className="mt-2">
+              Visualization Framework:
+              <i className="ml-2 text-teal-300">Apache Echarts, Chart.js</i>
+            </div>
+            <div className="mt-2">
+              Version Control: <i className="ml-2 text-green-400">Git</i>
+            </div>
+            <div className="mt-2">
+              DevOps: <i className="ml-2 text-blue-400">Kubernetes, Jenkins</i>
+            </div>
+            <div className="mt-2">
+              Others:
+              <i className="ml-2 text-yellow-200">
+                Common Design Patterns ,Basic algorithm,Markdown
+              </i>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="flex justify-between items-center m-2"></div>
     </Hero>
