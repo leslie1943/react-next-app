ee74fd5484af769749c35542da8b10725f0cbef7
leslie1943 <leslie43@sina.com>
2025-02-05 09:18:03 +0800

    others

--- src/app/(hero)/projects/page.tsx
+++ src/app/(hero)/projects/page.tsx
@@ -3,5 +3,15 @@ import imgSrc from "../../../../public/projects.jpg";
 import Hero from "@/components/hero";
 
 export default function Page() {
-  return <Hero imgUrl={imgSrc} altTxt="Scale Image" content="Projects"></Hero>;
+  return (
+    <Hero imgUrl={imgSrc} altTxt="Scale Image" content="">
+      <div className="flex items-center justify-center m-4">
+        <div className="text-white text-3xl m-10">Workforce</div>
+        <div className="text-white text-3xl m-10">Transparent</div>
+        <div className="text-white text-3xl m-10">EPRO</div>
+        <div className="text-white text-3xl m-10">EPRO-Operation</div>
+        <div className="text-white text-3xl m-10">BCRS</div>
+      </div>
+    </Hero>
+  );
 }
